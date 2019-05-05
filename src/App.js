bfc5f54aa6b0ec997373f7a6db7dcdae7bd20160
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2019-05-05 13:00:14 -0600

    Adding dependencies and editing app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+require('dotenv').config()
+const port = process.env.PORT || 3000
+const url = process.env.API_URL
+console.log(url)
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoading: true,
+      weather: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(url + '/weather')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          isLoading: false,
+          blocks: data
+        })
+        console.log(this.state.blocks)
+      })
+      .catch(console.error)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
